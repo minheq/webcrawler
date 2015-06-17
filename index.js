c794d5b8f453fbd962d681cb2deddfb939b4664a
Vu Hoang Minh <vuhminh.ubuntu@gmail.com>
2015-06-17 16:13:52 +0800

    Reconfigured to localhost scraping jpit

--- index.js
+++ index.js
@@ -1,87 +1,59 @@
 var express = require('express');
 var fs = require('fs');
 var request = require('request');
+var http = require('http');
 var cheerio = require('cheerio');
 var app     = express();
 
+
 app.get('/scrape', function(req, res){
 
-  //All the web scraping magic will happen here
-  
-  listing = 'http://www.renotalk.com/directory-listing/category/2/Interior-Design-Contractors';
-  
-  request(listing, function(error, response, html){
-    if(!error) { 
-      var $ = cheerio.load(html);
-      $('.listing-wrapper message_box').filter(function(){
-        var data = $(this);
-        var list = data.children();
-        console.log(list);
-      }
+    //First page to enter with the list
+    var arr = [];
+    arr.push('http://localhost:8080/en/node/352/22.html');
+    arr.push('http://localhost:8080/en/node/352/222679.html?page=1');
+    arr.push('http://localhost:8080/en/node/352/224658.html?page=2');
+    arr.push('http://localhost:8080/en/node/352/229ba9.html?page=3');
+    arr.push('http://localhost:8080/en/node/352/22fdb0.html?page=4');
+    arr.push('http://localhost:8080/en/node/352/22af4d.html?page=5');
+
+    for(var i = 0, limit = arr.length; i < limit; i++) {
+    //send HTTP request to the server to receive HTML
+      request(arr[i], function(error, response, html){
+
+          if(!error){
+              //load HTML with cheerio for selector query
+              var $ = cheerio.load(html);
+              
+
+              //grab the URL from the list
+              $('.view-field-node_title').filter(function(){
+                  var data = $(this);
+                  // console.log('http://localhost:8080/en/node' + data.children().children()[1].attribs.href.substring(13));
+                  var innerURL = 'http://localhost:8080/en/node' + data.children().children()[1].attribs.href.substring(13);
+                  
+
+                  request(innerURL, function(error2, response2, html2){
+                    $ = cheerio.load(html2);
+                    
+                    $('h1.title').filter(function(){
+                      data = $(this);
+                      // console.log(data[0].children[0].data);
+                    });
+
+                    $('p.paleGreen').filter(function(){
+                      data = $(this);
+                      console.log(data[0].children[0]);
+                    });
+
+                  });
+              })
+          }
+      })  
     }
-  }
-
-  /*
-  request(url, function(error, response, html){
-
-    if(!error) { 
-      var $ = cheerio.load(html);
-      //initialize what is being scraped
-      var web, companyName, phone, address, email;
-      var json = {
-        companyName: "",
-        web: "",
-        phone: "",
-        address: "",
-        email: ""
-      };
-      
-      //extracting data from the DOM 
-      $('.merchant-contact-web').filter(function(){
-        var data = $(this);
-
-        web = data.children().first().text();
-        json.web = web;
-        console.log(web);
-      })
-
-      $('.merchant-contact-phone').filter(function(){
-        var data = $(this);
-        json.phone = data.text();
-      })
-
-      $('.merchant-contact-address').filter(function(){
-        var data = $(this);
-        json.address = data.text();
-      })
-
-      companyName = $("input[name='list_name']").val();
-      json.companyName = companyName;
-      email = $("input[name='email_id']").val();
-      json.email = email;
-
-      console.log(companyName);
-      console.log(email);
-
-    } else {
-      console.log(error);
-    }
-
-    //save the json to a file
-    fs.writeFile('renotalk.json', JSON.stringify(json, null, 4), function(err){
-    console.log('File successfully written! - Check your project directory for the renotalk.json file'); */
-  })
-
-  //send response
-  res.send('Check your console!');
-  })
-
-  
+    
 })
 
-
 app.listen('8081')
-
 console.log('Magic happens on port 8081');
-
 exports = module.exports = app;
